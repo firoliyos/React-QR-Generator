8b322ceaad07860b731018197fe34186d337f745
firoliyos <firatesfaye44@gmail.com>
2024-10-21 14:57:03 +0300

    index

--- qr-generator/src/components/index.jsx
+++ qr-generator/src/components/index.jsx
@@ -2,25 +2,12 @@ import {useState} from "react"
 import {QRCodeCanvas} from "qrcode.react"
 
 export default function QrCodeGenerator() {
+   
 
-    const [text, setText] = useState("")
-
-    return(
-      <div style={{ textAlign: 'center', padding: '50px' }}>
-        <h1>QR Code Generator</h1>
-        <input 
-          type="text"
-          placeholder="Enter Text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          style={{ padding: '10px', marginBottom: '20px', width: '50%', height: '50px', backgroundColor: "lightGreen" }}
-        />
-
-        <div style={{ marginTop: '20px' }} >
-          {
-            text && <QRCodeCanvas value={text} size={300} bgColor="lightGreen" fgColor="black" />
-          }
-        </div>
-      </div>  
-    )
+  return(
+    <div className="main-container">
+        <h1>QR CODE GENERATOR</h1>
+       
+    </div>
+  )
 }
